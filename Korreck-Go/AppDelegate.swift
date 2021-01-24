0787e631a4f7c4e9c43b23ccf2aa3978905f64c8
Jeremiah Korreck <korreckj328@gmail.com>
2021-01-24 18:32:40 -0500

    Initial Commit

---
+++ Korreck-Go/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Korreck-Go
+//
+//  Created by Jeremiah Korreck on 1/24/21.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
